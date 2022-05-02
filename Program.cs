dda61df02b8850b99a870b058a3abffa4e4d85fa
Vladimir <71398553+BackSN-prog@users.noreply.github.com>
2022-05-02 14:12:18 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenGLDemoV;
+using System;
+using System.Windows.Forms;
+
+namespace OpenGLDemo
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        [Obsolete]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
